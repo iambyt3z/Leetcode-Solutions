353d98e4286fec09d9b1c0df36ddeaed56a8eecf
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2023-09-21 01:24:58 +0530

    Time: 60 ms (58.65%), Space: 40.6 MB (22.33%) - LeetHub

---
+++ 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int len=arr.size();
+        if(len==1) return 1;
+
+        int res=1, l=0, r=0;
+        bool dir=true;
+
+        while(l<=r && r < len-1) {
+            if(l==r) {
+                if(arr[r] > arr[r+1]) {
+                    r++;
+                    dir=true;
+                }
+
+                else if(arr[r] < arr[r+1]) {
+                    r++;
+                    dir=false;
+                }
+
+                else {
+                    r++;
+                    l++;
+                }
+
+                res = max(res, r-l+1);
+                continue;
+            }
+
+            if(
+                (dir && arr[r] < arr[r+1]) ||
+                (!dir && arr[r] > arr[r+1])
+            ) {
+                r++;
+                dir = !dir;
+                res = max(res, r-l+1);
+            }
+
+            else  {
+                l=r;
+            }
+        }
+
+        return res;
+    }
+};
