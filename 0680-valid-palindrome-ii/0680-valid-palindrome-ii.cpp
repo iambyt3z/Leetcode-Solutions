1e9fcfd939ee03e3278136cc2b27f0e77a6824b0
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2023-09-26 05:53:07 +0530

    Time: 61 ms (12.42%), Space: 20 MB (73.95%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    bool isPalindrome(int l, int r, string &s) {
-        while(l <= r) {
-            if(s[l] != s[r]) return false;
-            l++;
-            r--;
-        }
+    bool solve(int l, int r, int count, string &s) {
+        if(l > r) return (count <= 1);
+        if(count > 1) return false;
 
-        return true;
+        if(s[l] == s[r]) return solve(l+1, r-1, count, s);
+
+        return solve(l+1, r, count+1, s) || solve(l, r-1, count+1, s);
     }
 
     bool validPalindrome(string s) {
-        int l=0, r=s.length()-1;
-
-        while(l<=r) {
-            if(s[l] != s[r]) {
-                return 
-                    isPalindrome(l, r-1, s) ||
-                    isPalindrome(l+1, r, s);
-            }
-
-            l++;
-            r--;
-        }
-
-        return true;
+        return solve(0, s.length()-1, 0, s);
     }
 };
