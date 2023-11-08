3c76d4689e312c965573ee3b4950918b611b3dc4
Siddhant Thakare <75215762+iambyt3z@users.noreply.github.com>
2023-11-07 18:20:50 -0800

    Time: 724 ms (5.01%), Space: 18.5 MB (5.12%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,49 +1,8 @@
 class Solution {
 public:
-    // void dfs(int l, int r, vector<int> &cuts, int &res) {
-    //     if(r-l == 1)
-    //         return ;
-
-    //     if(l >= cuts[cuts.size()-1] || r <= cuts[0])
-    //         return ;
-
-    //     int i = 0, j = cuts.size()-1;
-    //     int cutIndex = -1, diffFromMid = r-l;
-
-    //     while(i <= j) {
-    //         int mid = i + (j-i)/2;
-            
-    //         if(cuts[mid] <= l) {
-    //             i = mid+1;
-    //             continue;
-    //         }
-
-    //         if(cuts[mid] >= r) {
-    //             j = mid-1;
-    //             continue;
-    //         }
-
-    //         int diff = l+(r-l)/2 - cuts[mid];
-    //         if(abs(diff) < diffFromMid) {
-    //             diffFromMid = abs(diff);
-    //             cutIndex = mid;
-    //         }
-
-    //         if(diff == 0) break;
-    //         else if(diff > 0) i = mid+1;
-    //         else j = mid-1;
-    //     }
-
-    //     if(cutIndex == -1) return ;
-
-    //     res += (r-l); 
-    //     dfs(l, cuts[cutIndex], cuts, res);
-    //     dfs(cuts[cutIndex], r, cuts, res);
-    // }
-
     map<pair<int, int>, int> dp;
 
-    int solve(int l, int r, vector<int> &cuts) {
+    int solve(int l, int r, int i, int j, vector<int> &cuts) {
         if(r-l == 1) 
             return 0;
 
@@ -53,12 +12,16 @@ public:
         if(dp.find({l, r}) != dp.end()) return dp[{l, r}];
 
         int res = INT_MAX;
-        for(int i=0; i<cuts.size(); i++) {
-            if(cuts[i] > l && cuts[i] < r)
-                res = min(
+
+        for(int ind = i; ind <= j; ind++) {
+            if(cuts[ind] > l && cuts[ind] < r) {
+                res = min (
                     res, 
-                    (r-l) + solve(l, cuts[i], cuts) + solve(cuts[i], r, cuts)
-                );
+                    (r-l) + 
+                        solve(l, cuts[ind], i, ind, cuts) + 
+                        solve(cuts[ind], r, ind, j, cuts)
+                ); 
+            } 
         }
 
         if(res == INT_MAX)
@@ -71,8 +34,6 @@ public:
         sort(cuts.begin(), cuts.end());
         dp.clear();
 
-        cout << "break";
-
-        return solve(0, n, cuts);;
+        return solve(0, n, 0, cuts.size()-1, cuts);;
     }
 };
